cc26d41eaad0fad86a2f79a6d968afcb6ea1ab0c
Matthieu <matthieupasserel@gmail.com>
2018-08-02 10:26:38 +0200

    Initial Commit

---
+++ Intro à CoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intro à CoreML
+//
+//  Created by Matthieu PASSEREL on 02/08/2018.
+//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
