a881f49ee683c257d1a46c5b00626fd9289d390c
Thierry Huu <huuth@macbook-pro-de-thierry.local>
2018-10-26 00:38:23 +0200

    Faire une requête avec CoreML et Vision

--- Intro à CoreML/Controller/CoreMLController.swift
+++ Intro à CoreML/Controller/CoreMLController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class CoreMLController: UIViewController {
     
@@ -17,10 +19,34 @@ class CoreMLController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         imageView.image = image
+        
+        if let img = image, let data = img.pngData() {
+            request(data: data)
+        }
     }
     
     @IBAction func backAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func request(data: Data) {
+        do {
+            let modele = try VNCoreMLModel(for: MobileNet().model)
+            let requete = VNCoreMLRequest(model: modele, completionHandler: response)
+            let handler = VNImageRequestHandler(data: data, options: [:])
+            try handler.perform([requete])
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func response(_ requete: VNRequest, _ error: Error?) {
+        if let resultats = requete.results as? [VNClassificationObservation] {
+            for resultat in resultats {
+                print("Résultat : " + resultat.identifier + " confiance \(Int(resultat.confidence * 100)) %")
+            }
+        }
     }
 }
